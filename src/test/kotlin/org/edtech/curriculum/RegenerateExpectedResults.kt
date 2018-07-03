52263351473c6f8024a4366a8a78cfd75b5a4159
Stefan Jonasson <stefan.jonasson@ist.com>
2018-07-03 10:30:14 +0200

    Added support for multiple versions

--- src/test/kotlin/org/edtech/curriculum/RegenerateExpectedResults.kt
+++ src/test/kotlin/org/edtech/curriculum/RegenerateExpectedResults.kt
@@ -3,12 +3,20 @@ package org.edtech.curriculum
 import java.io.File
 
 fun main(args : Array<String>) {
-    val subjectFileGenerator = SubjectFileGenerator(File("./src/test/resources/valid/2018-07-02"), File("./src/test/resources/opendata/2018-07-02"))
+    val versions = File("./src/test/resources/valid/").listFiles()
+
     if (args.isEmpty()) {
-        subjectFileGenerator.regenerate()
+        versions.forEach {
+            val subjectFileGenerator = SubjectFileGenerator(it, File("./src/test/resources/opendata/${it.name}"))
+            subjectFileGenerator.regenerate()
+        }
     } else if (args.size == 2) {
+
         if (SyllabusType.values().any { it.name == args[0] }) {
-            subjectFileGenerator.generateOneSubject(SyllabusType.valueOf(args[0]), args[1])
+            versions.forEach {
+                val subjectFileGenerator = SubjectFileGenerator(it, File("./src/test/resources/opendata/${it.name}"))
+                subjectFileGenerator.generateOneSubject(SyllabusType.valueOf(args[0]), args[1])
+            }
         }
     } else {
         println("ERROR: Incorrect parameter format.")
